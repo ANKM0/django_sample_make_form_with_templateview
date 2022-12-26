c86184c9a06e992e481e548e2f91b5c966ae5a02
ARFCAC <xingsantianzhong460@gmail.com>
2022-12-26 23:11:35 +0900

    #15 完了

--- django_sample/app1/views.py
+++ django_sample/app1/views.py
@@ -1,31 +1,30 @@
+from django.views.generic.edit import ModelFormMixin
 from django.views.generic import TemplateView, ListView
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 
 from .forms import TestDataModelForm
 from .models import TestData
 
 
-class IndexView(TemplateView):
+class IndexView(TemplateView, ModelFormMixin):
+    # https://docs.djangoproject.com/en/4.1/ref/class-based-views/mixins-single-object/
     template_name: str = "app1/index.html"
     form_class = TestDataModelForm
+    success_url = reverse_lazy('app1:list')
+    model = TestData
 
-    def get_context_data(self, **kwargs):
-        # get処理だけ書く
-        ctx = super().get_context_data(**kwargs)
-        ctx["form"] = self.form_class()
-        return ctx
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        return super().get(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs) -> HttpResponse:
-        form = self.form_class(request.POST)
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        self.object_list = self.get_queryset()
+        form = self.get_form()
         if form.is_valid():
-            form.save()
-            return redirect("app1:data_list")
+            return self.form_valid(form)
         else:
-            context = {
-                "error_list": form.errors,
-            }
-            return render(request, self.template_name, context)
+            return self.form_invalid(form)
 
 
 class DataListView(ListView):
